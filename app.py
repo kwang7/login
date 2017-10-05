750b7366247e88f70e585d2e698fa32377904569
Kelly Wang <kwang7@stuy.edu>
2017-10-04 21:56:02 -0400

    not done

--- app.py
+++ app.py
@@ -3,28 +3,38 @@ import os
 
 app = Flask(__name__)
 
+app.secret_key = os.urandom(32) #32 bits of random data as a string
+
+username = 'kelly'
+password = 'wang'
+
 @app.route("/")
 def hello_world():
-    if session['username'] == 'kelly':
-        if session['password'] == 'wang':
-            return render_template("hello.html")
-        else:
-            return render_template("goodbye.html")
-    else:
-        return render_template("goodbye.html")
-
-    app.secret_key = os.urandom(32) #32 bits of random data as a string
-    #print session
-    session['username']=request.args['username']
-    session['password']='wang'
+    
+    print session;
     print '_______________________________________________COOKIES GET USERNAME'
     print request.cookies.get('username')
+    print "-----------------------ARGS "
+    print request.args
+    print "-----------------------FORM "
+    print request.form
 
+    if request.args["username"] == "kelly":
+        if request.args['password'] == 'wang':
+            return render_template("form.html")
+        else:
+            return render_template("form.html", error = 'Wrong Password' )
+    else:
+        return render_template("form.html", error = "Wrong Username")
+    
+    session['username']=request.args['username']
+    session['password']='wang'
+    
     return render_template('form.html')
 
-#@app.route("/hello")
-#def hello():
-
+@app.route("/hello")
+def hello():
+    return 'hi'
 
 if __name__ == "__main__":
     app.debug = True
